295b3e74ae9b3cb28a69e3ccb331fcb03a3c1a50
Stephen A. Gonzalez <sag754@nyu.edu>
2020-03-30 11:30:31 -0400

    Added sound effect to ball collision.

--- Codelab1-sag754-HW6/Assets/Scripts/ActivateOnClick.cs
+++ Codelab1-sag754-HW6/Assets/Scripts/ActivateOnClick.cs
@@ -6,15 +6,23 @@ public class ActivateOnClick : MonoBehaviour
 {
     Rigidbody rb; //var for rigidbody
 
+    public AudioClip boop;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); //Get RigidBody Component
         rb.isKinematic = true; //Make it Kinematic (ie, doen't move with physics)
+        GetComponent<AudioSource>().clip = boop;
     }
 
     private void OnMouseDown() //if you click on the object
     {
         rb.isKinematic = false; //make it able to move w/ physics
+    }
+
+    void OnCollisionEnter()
+    {
+        GetComponent<AudioSource>().Play();
     }
 }
